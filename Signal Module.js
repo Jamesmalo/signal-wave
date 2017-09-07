cc14d957f1a1be50f13976f39db2d6b6d51c627b
Jamesmalo <9jmalo@stpatrick.org>
2017-09-07 14:44:45 -0500

    day 1

---
+++ Signal Module.js
@@ -0,0 +1,34 @@
+let Signal = function(){
+  let numberOfPoints = array.length;
+  let points = array;
+  let transformedPoints = array;
+  let range;
+  let period;
+
+  function getRange(){return range;}
+  function getPeriod(){return period;}
+  function getPointArray(){return points;}
+
+  function shiftX(){
+
+  }
+  function shiftY(){
+
+  }
+  function stretchX(){
+
+  }
+  function stretchY(){
+
+  }
+  function transformation(){
+
+  }
+  function outputOf(){
+
+  }
+  function calculateSpan(){
+
+  }
+  return{shiftX,shiftY,stretchY,stretchX,transformation,outputOf}
+}
