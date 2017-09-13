230faed9a3f3292018abbb3f8197147ca2cd3b94
Jamesmalo <9jmalo@stpatrick.org>
2017-09-13 14:46:17 -0500

    day 4

--- Signal Module.js
+++ Signal Module.js
@@ -10,23 +10,28 @@ let Signal = function(){
   function getPointArray(){return points;}
 
   function shiftX(value){
-    transformedPoints.getX()=points.getx+value;
-  }
+      
+      let tempx = ;
+      let tempy = ;
+      tempx += ;
+      let temppoint= ;
+      transformedPoints[a] = temppoint;
+    }
   function shiftY(value){
     transformedPoints.getY()=points.gety+value;
   }
   function stretchX(value){
-    Points.getX()=points.getx*value;
+    points.getX()=points.getx*value;
   }
   function stretchY(value){
-    Points.getY()=points.gety*value;
+    points.getY()=points.gety*value;
   }
   function transformation(){
     return transformedPoints;
   }
   function outputOf(xvalue){
     for(let x = 0;x < points.length;x++){
-      
+
     }
   }
   function calculateSpan(){
