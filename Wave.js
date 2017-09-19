423950f0905a6c4bef3bee168806f39dc7736373
Jamesmalo <9jmalo@stpatrick.org>
2017-09-19 14:44:36 -0500

    Finished

--- Wave.js
+++ Wave.js
@@ -1,68 +1,82 @@
-let Signal = function(){
+const Point =require("./Point.js");
+
+const Wave = function(array){
   let numberOfPoints = array.length;
   let points = array;
   let transformedPoints = array;
-  let range =calculateSpan(points,"y");
-  let period;
+  let range = calculateSpan(array,"y");
+  let period = calculateSpan(array, "x");
 
   function getRange(){return range;}
   function getPeriod(){return period;}
   function getPointArray(){return points;}
 
   function shiftX(value){
-     for(var=0;a<numberOfPoints; a++)
+     for(var a=0;a<numberOfPoints; a++){
       let tempx = transformedPoints[a].getX();
-      let tempy = transformedpoints[a].getY();
+      let tempy = transformedPoints[a].getY();
       tempx += value;
       transformedPoints[a] = new Point(tempx,tempy);
+        }
     }
   function shiftY(value){
-      for(var=0;a<numberOfPoints; a++)
+      for(vara=0;a<numberOfPoints; a++){
        let tempx = transformedPoints[a].getX();
-       let tempy = transformedpoints[a].getY();
+       let tempy = transformedPoints[a].getY();
        tempy += value;
        transformedPoints[a] = new Point(tempx,tempy);
+   }
   }
   function stretchX(value){
-      for(var=0;a<numberOfPoints; a++)
+      for(vara=0;a<numberOfPoints; a++){
        let tempx = transformedPoints[a].getX();
-       let tempy = transformedpoints[a].getY();
+       let tempy = transformedPoints[a].getY();
        tempx += value;
        transformedPoints[a] = new Point(tempx,tempy);
+   }
   }
   function stretchY(value){
-      for(var=0;a<numberOfPoints; a++)
+      for(var a=0;a<numberOfPoints; a++){
        let tempx = transformedPoints[a].getX();
-       let tempy = transformedpoints[a].getY();
+       let tempy = transformedPoints[a].getY();
        tempy += value;
        transformedPoints[a] = new Point(tempx,tempy);
+   }
   }
-  function transformation(){
+  function transform(){
     return transformedPoints;
   }
   function outputOf(xvalue){
-    for(let a = 0;a < points.length;a++){
-        if(point.getX()[a] == xvalue){
-            return points.getY();
-        }
-    }
-    console.log("The x value has no y value.");
+    let position = xvalue % period;
+    return points[position].getY();
   }
   function calculateSpan(array,coor){
-      if(coor=="x"){
-          let min=points[0].getX();
-          let max=points[0];
-         for(let a = 1;a < points.length;a++){
-             if(points[a].getX() >max){
-                 max = points[a];
-             }
-             if(<min){
+      let min=points[0];
+      let max=points[0];
 
+        if(coor == "x"){
+         for(let a = 1;a < numberOfPoints;a++){
+             if(points[a].getX() < min){
+                 min = points[a].getX();
+             }
+             if(points[a].getX() > max){
+                 max = points[a].getX();
              }
          }
       }
-      if(){}
+      return max-min;
+        if(coor == "y"){
+            for(let a = 1;a < numberOfPoints;a++){
+                if(points[a].getY() < min){
+                    min = points[a].getY();
+                }
+                if(points[a].getY() > max){
+                    max = points[a].getY();
+                }
+            }
+        }
      return max-min;
   }
-  return{shiftX,shiftY,stretchY,stretchX,transformation,outputOf}
+  return{shiftX,shiftY,stretchY,stretchX,transform,outputOf,range,period}
 }
+module.exports = Wave;
